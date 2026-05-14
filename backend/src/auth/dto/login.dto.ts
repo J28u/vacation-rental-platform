7d3aeed42995983af55702f29f6b56d1c1e347e8
J28u <judithcecchin@gmail.com>
2026-05-14 20:37:24 +0200

    :adhesive_bandage: Override password property in LoginDto to prevent IsStrongPassword validation error

--- backend/src/auth/dto/login.dto.ts
+++ backend/src/auth/dto/login.dto.ts
@@ -1,4 +1,9 @@
 import { RegisterDto } from './register.dto';
-import { OmitType } from '@nestjs/swagger';
+import { OmitType, ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
 
-export class LoginDto extends OmitType(RegisterDto, ['name'] as const) {}
+export class LoginDto extends OmitType(RegisterDto, ['name'] as const) {
+  @IsString()
+  @ApiProperty({ example: "GfyTui0ç6'$42;:1_*" })
+  password!: string;
+}
