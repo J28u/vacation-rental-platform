888cc99d51afc53fc9fdaebb84c800f55162becf
J28u <judithcecchin@gmail.com>
2026-05-14 20:47:40 +0200

    :bug: Fix raw query used to assert prisma connection to MySQL

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -15,7 +15,7 @@ export class PrismaService
   async onModuleInit() {
     try {
       await this.$connect();
-      await this.$queryRaw`SELECT 1`;
+      await this.$queryRaw`SELECT 1;`;
       console.log('✅ Prisma connected to MySQL');
     } catch (error) {
       console.error('❌ Prisma connection error:', error);
